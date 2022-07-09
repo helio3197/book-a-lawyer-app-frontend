6f43b2616b34b8dcf85bc4e57cfa5004f79ccfa8
Kenny <kennysalazar3197@gmail.com>
2022-07-08 19:40:23 -0500

    Remove boilerplate and initialize sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
