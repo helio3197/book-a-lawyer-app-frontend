2b6f547d0543ecc4c6a990418f855c7a68e69bb9
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-10 21:38:12 +0300

    chnage the App to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
+import Home from './Components/Home';
 
-function App() {
-  return (
-    <>
-      <Sidebar />
-      <div className="App" />
-    </>
-  );
-}
+const App = () => (
+  <>
+    {/* <Sidebar /> */}
+    <div className="App" />
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
