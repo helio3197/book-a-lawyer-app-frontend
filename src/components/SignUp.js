17775ce7bf3cfb492d45aba98c5b08cc6465de88
Kenny <kennysalazar3197@gmail.com>
2022-07-09 21:02:31 -0500

    Add avatar preview feature to sign up page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,20 +1,77 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { GrFormClose } from 'react-icons/gr';
 import { signUp } from '../redux/auth/auth';
+import defaultAvatar from '../assets/images/profile-pic.png';
 
 const SignUp = () => {
+  const formInitialState = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+    avatar: {
+      value: '',
+      preview: defaultAvatar,
+      file: null,
+    },
+  };
   const authState = useSelector((state) => state.auth);
+  const [formState, setFormState] = useState(formInitialState);
   const dispatch = useDispatch();
 
   console.log(authState);
+
+  const removeSelectedPicture = () => {
+    setFormState((state) => ({
+      ...state,
+      avatar: formInitialState.avatar,
+    }));
+  };
+
+  const previewHandler = (e) => {
+    if (e.target.value) {
+      const file = e.target.files[0];
+      setFormState((state) => ({
+        ...state,
+        avatar: {
+          value: e.target.value,
+          preview: URL.createObjectURL(file),
+          file,
+        },
+      }));
+      return undefined;
+    }
+
+    removeSelectedPicture();
+    return undefined;
+  };
 
   return (
     <Container fluid>
       <h1>Sign up</h1>
       <Form>
+        <div className="avatar mb-3">
+          <div className="preview">
+            <img src={formState.avatar.preview} alt="avatar preview" />
+          </div>
+          <div className="image-upload">
+            <div className="file-picker">
+              <label htmlFor="avatar">Upload picture</label>
+              <input id="avatar" type="file" onChange={previewHandler} value={formState.avatar.value} accept="image/jpeg, image/png, image/gif" />
+            </div>
+            <div className="file-selection">
+              <small>{formState.avatar.file ? formState.avatar.file.name : 'No file chosen...'}</small>
+              <button type="button" className={formState.avatar.value ? '' : 'd-none'} onClick={removeSelectedPicture}>
+                <GrFormClose />
+              </button>
+            </div>
+          </div>
+        </div>
         <Form.Group controlId="name">
           <Form.Label visuallyHidden>Name</Form.Label>
           <Form.Control placeholder="Name" />
@@ -35,7 +92,7 @@ const SignUp = () => {
           <Form.Control type="password" placeholder="Password" />
           <Form.Control.Feedback type="invalid">Custom Error</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group controlId="confirm_password">
+        <Form.Group controlId="password_confirmation">
           <Form.Label visuallyHidden>Confirm password</Form.Label>
           <Form.Control type="password" placeholder="Confirm password" />
           <Form.Control.Feedback type="invalid">Custom Error</Form.Control.Feedback>
