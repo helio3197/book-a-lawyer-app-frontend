f66f6567d7f7079037681234e4807754b5f9e5a7
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-10 21:38:41 +0300

    add sidebar content

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavLink } from 'react-router-dom';
+import CancelIcon from '@mui/icons-material/Cancel';
+// import Button from '@material-ui/core/Button';
+import '../App.css';
+import PropTypes from 'prop-types';
+// import { NavLink } from 'react-bootstrap';
 
-const Sidebar = () => (
-  <h1>This is a nav item</h1>
+const Sidebar = ({ children }) => (
+  <main>
+    <header className="d-flex">
+      <h1>My Logo</h1>
+    </header>
+    <div>
+      <MenuIcon />
+      <CancelIcon />
+    </div>
+    <nav className="links">
+      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} to="/" className="link-item">Home</NavLink>
+      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reserve" className="link-item">Reserve</NavLink>
+      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reservations" className="link-item">Reservations</NavLink>
+    </nav>
+    { children }
+  </main>
 );
+
+Sidebar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Sidebar;
