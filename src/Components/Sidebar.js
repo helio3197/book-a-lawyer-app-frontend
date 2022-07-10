cf5c2bef67128676b36c6fd6ca9497071a1bb6a0
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-11 00:02:21 +0300

    add toggle

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from 'react-router-dom';
 import CancelIcon from '@mui/icons-material/Cancel';
-// import Button from '@material-ui/core/Button';
 import '../App.css';
+import './Sidebar.css';
 import PropTypes from 'prop-types';
-// import { NavLink } from 'react-bootstrap';
 
-const Sidebar = ({ children }) => (
-  <main>
-    <header className="d-flex">
-      <h1>My Logo</h1>
-    </header>
-    <div>
-      <MenuIcon />
-      <CancelIcon />
-    </div>
-    <nav className="links">
-      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} to="/" className="link-item">Home</NavLink>
-      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reserve" className="link-item">Reserve</NavLink>
-      <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reservations" className="link-item">Reservations</NavLink>
-    </nav>
-    { children }
-  </main>
-);
+const Sidebar = ({ children }) => {
+//   const [show, setShow] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+  const onClick = () => setShowResults(!showResults);
+
+  return (
+    <>
+
+      <main>
+        <header className="d-flex">
+          <div className="top-head">
+            <h1>My Logo</h1>
+            <div className="menu-icons">
+              {/* <span>
+                <MenuIcon onClick={() => setShow((prev) => !prev)} />
+                { show && <CancelIcon />}
+
+              </span> */}
+
+              <div>
+                <button type="button" onClick={onClick}>{ showResults ? <MenuIcon /> : <CancelIcon /> }</button>
+                {/* <input type="submit" value="Search" onClick={onClick} />
+                { showResults ? <MenuIcon /> : <CancelIcon /> } */}
+              </div>
+              {/* <CancelIcon className="close-icon" /> */}
+            </div>
+          </div>
+          <nav className="links">
+            <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} to="/" className="link-item">Home</NavLink>
+            <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reserve" className="link-item">Reserve</NavLink>
+            <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reservations" className="link-item">Reservations</NavLink>
+          </nav>
+        </header>
+        { children }
+      </main>
+    </>
+  );
+};
+
+// const Results = () => (
+//   <MenuIcon />
+// );
 
 Sidebar.propTypes = {
   children: PropTypes.node.isRequired,
