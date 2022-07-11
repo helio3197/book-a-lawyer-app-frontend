6d20c1d9416518ce52613771a9ea954a846fe657
Kenny <kennysalazar3197@gmail.com>
2022-07-11 14:19:42 -0500

    Initialize Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { useSelector } from 'react-redux';
+import Welcome from './Welcome';
 
-const Home = () => (
-  <>
-    <h1>HOMEPAGE PLACEHOLDER</h1>
-  </>
-);
+const Home = () => {
+  const userSignedIn = useSelector((state) => state.auth.userSignedIn);
+
+  return (
+    <Container fluid className="p-0">
+      <Container fluid className="py-2">
+        {userSignedIn
+          ? <h1>signed_in</h1>
+          : <Welcome />}
+      </Container>
+    </Container>
+  );
+};
 
 export default Home;
