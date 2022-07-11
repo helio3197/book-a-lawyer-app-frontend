140508f0d738010703e129274d900109255f7ea3
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-11 10:01:30 +0300

    add hide and show for themenu list on mobile

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,10 +7,14 @@ import './Sidebar.css';
 import PropTypes from 'prop-types';
 
 const Sidebar = ({ children }) => {
-//   const [show, setShow] = useState(false);
-  const [showResults, setShowResults] = useState(false);
-  const onClick = () => setShowResults(!showResults);
+  const [show, setShow] = useState(false);
+  const [showResults, setShowResults] = useState(true);
+  //   const onClick = () => setShowResults(!showResults);
 
+  const onClick = () => {
+    setShowResults(!showResults);
+    setShow(!show);
+  };
   return (
     <>
 
@@ -19,25 +23,19 @@ const Sidebar = ({ children }) => {
           <div className="top-head">
             <h1>My Logo</h1>
             <div className="menu-icons">
-              {/* <span>
-                <MenuIcon onClick={() => setShow((prev) => !prev)} />
-                { show && <CancelIcon />}
-
-              </span> */}
-
               <div>
-                <button type="button" onClick={onClick}>{ showResults ? <MenuIcon /> : <CancelIcon /> }</button>
-                {/* <input type="submit" value="Search" onClick={onClick} />
-                { showResults ? <MenuIcon /> : <CancelIcon /> } */}
+                <button type="button" onClick={onClick}>{ showResults ? <MenuIcon /> : <CancelIcon />}</button>
               </div>
-              {/* <CancelIcon className="close-icon" /> */}
             </div>
           </div>
+          {show
+          && (
           <nav className="links">
             <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} to="/" className="link-item">Home</NavLink>
             <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reserve" className="link-item">Reserve</NavLink>
             <NavLink style={({ isActive }) => ((isActive) ? { color: 'red' } : {})} activeClassName="is-active" to="/Reservations" className="link-item">Reservations</NavLink>
           </nav>
+          )}
         </header>
         { children }
       </main>
