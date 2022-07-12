7e257e56630348e9a7a8b5766cf6832c991154b1
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-12 20:55:07 +0300

    add lawyer detaisls page on mobile and large device

--- src/components/home/Details.js
+++ src/components/home/Details.js
@@ -8,11 +8,27 @@ const Detail = () => (
     <section className="detail-section">
       <div className="top-details">
         <img src={Logo} alt="Profle" className="profile-pic" />
-        <p className="profile-name">
-          <strong>Name:</strong>
-          {' '}
-          Richard Opiyo
-        </p>
+        <ul className="lawyer-items">
+          <div className="inner-listing">
+            <li><strong>Name:</strong></li>
+            <li className="inner-listing2"> Richard Opiyo</li>
+          </div>
+          <div className="inner-listing">
+            <li><strong>Phone No:</strong></li>
+            <li className="inner-listing2"> 0790837592</li>
+          </div>
+          <div className="inner-listing">
+            <li><strong>Location:</strong></li>
+            <li className="inner-listing2"> Nairobi, Kenya</li>
+          </div>
+          <div className="inner-listing">
+            <li><strong>Charges:</strong></li>
+            <li className="inner-listing2"> $45/hr</li>
+          </div>
+        </ul>
+      </div>
+      <div className="bottom-details">
+        <p className="bio-head">Biography</p>
         <p className="bio">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
@@ -21,25 +37,6 @@ const Detail = () => (
           It has survived not only five centuries, but also the leap into electronic
           {' '}
         </p>
-      </div>
-      <div className="bottom-details">
-        <ul className="lawyer-items">
-          <li>
-            <strong>Phone Number:</strong>
-            {' '}
-            0790837592
-          </li>
-          <li>
-            <strong>Location:</strong>
-            {' '}
-            Nairobi, Kenya
-          </li>
-          <li>
-            <strong>Charges:</strong>
-            {' '}
-            $45/hr
-          </li>
-        </ul>
         <Button href="/reserve" variant="success" size="lg">Reserve</Button>
       </div>
     </section>
