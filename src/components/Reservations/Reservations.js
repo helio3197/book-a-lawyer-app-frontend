8fec099b51ce6394bf4e789f7a3ace3dfee891f2
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-14 20:55:51 +0300

    add reservations

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fechReservations } from '../../redux/reservations/reservations';
 
-const Reservations = () => (
-  <>
-    <h1>This is the Reservations page!</h1>
-  </>
-);
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const reservationState = useSelector((state) => state.reservations);
+
+  useEffect(() => {
+    // if (reservationState !== 'completed') {
+    dispatch(fechReservations());
+    console.log(reservationState);
+    // }
+  }, []);
+  // console.log(reservationState);
+  return (
+    <>
+      <h1>This is the Reservations page!</h1>
+    </>
+  );
+};
 
 export default Reservations;
