deed8ff5d4fdc980514e83b8dc7da686e3f4270c
Kenny <kennysalazar3197@gmail.com>
2022-07-14 17:52:20 -0500

    Add remove lawyer behavior to dropdown list

--- src/components/reserve/Reserve.js
+++ src/components/reserve/Reserve.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
+import { IoClose } from 'react-icons/io5';
 import CustomDropMenu from './CustomDropMenu';
 import { getLawyers } from '../../redux/lawyers/lawyersIndex';
 
@@ -18,8 +20,12 @@ const Reserve = () => {
   const { lawyers } = lawyersState;
   const mediaQuerySm = window.matchMedia('(min-width: 600px)');
   const [dropdownPosition, setDropdownPosition] = useState(mediaQuerySm.matches ? 'end' : 'down');
+  const initialReservationData = {
+    lawyer_id: selectedLawyer,
+  };
+  const [reservationData, setReservationData] = useState(initialReservationData);
 
-  console.log(selectedLawyer);
+  console.log(reservationData);
 
   useEffect(() => {
     if (lawyersState.status === 'failed') {
@@ -45,31 +51,54 @@ const Reserve = () => {
     };
   }, []);
 
+  const removeSelectedLawyer = (id) => {
+    const lawyerToRemove = lawyers.find((lawyer) => lawyer.id === +id);
+    const clickHandler = () => setReservationData((state) => ({ ...state, lawyer_id: null }));
+
+    return (
+      <Button type="button" variant="secondary" className="d-flex align-items-center gap-2" onClick={clickHandler}>
+        <img src={lawyerToRemove.avatar_url} alt={lawyerToRemove.name} style={{ width: '40px', height: '40px' }} className="lawyer-thumbnail" />
+        <p className="m-0 text-truncate">{lawyerToRemove.name}</p>
+        <IoClose className="text-light fs-4" />
+      </Button>
+    );
+  };
+
   return (
     <Container fluid className="h-100 reserve-bg d-flex">
       <Container fluid="sm" className="py-3 border rounded form-width-sm shadow my-auto bg-light">
         <Form className="reserve-form">
-          <Dropdown drop={dropdownPosition}>
-            <Dropdown.Toggle>
-              Select lawyer
-            </Dropdown.Toggle>
-            <Dropdown.Menu as={CustomDropMenu}>
-              {lawyers?.map((lawyer, index) => (
-                <Dropdown.Item eventKey={index + 1} key={lawyer.id} id={lawyer.name} className="d-flex gap-2 align-items-center">
-                  <img src={lawyer.avatar_url} alt={lawyer.name} className="lawyer-thumbnail" />
-                  <p className="m-0 text-truncate">{lawyer.name}</p>
-                </Dropdown.Item>
-              ))}
-              {lawyersState.status === 'fetching'
-                && (
-                  <Dropdown.Item eventKey="1" className="text-center">
-                    <Spinner animation="border" variant="primary" role="status" className="my-auto">
-                      <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                  </Dropdown.Item>
-                )}
-            </Dropdown.Menu>
-          </Dropdown>
+          {(reservationData.lawyer_id && lawyers)
+            ? removeSelectedLawyer(reservationData.lawyer_id)
+            : (
+              <Dropdown
+                drop={dropdownPosition}
+                onSelect={(_, e) => setReservationData((state) => ({
+                  ...state,
+                  lawyer_id: e.currentTarget.dataset.lawyerid,
+                }))}
+              >
+                <Dropdown.Toggle>
+                  Select lawyer
+                </Dropdown.Toggle>
+                <Dropdown.Menu as={CustomDropMenu}>
+                  {lawyers?.map((lawyer, index) => (
+                    <Dropdown.Item eventKey={index + 1} data-lawyerid={lawyer.id} key={lawyer.id} id={lawyer.name} className="d-flex gap-2 align-items-center">
+                      <img src={lawyer.avatar_url} alt={lawyer.name} className="lawyer-thumbnail" />
+                      <p className="m-0 text-truncate">{lawyer.name}</p>
+                    </Dropdown.Item>
+                  ))}
+                  {lawyersState.status === 'fetching'
+                    && (
+                      <Dropdown.Item eventKey="1" className="text-center">
+                        <Spinner animation="border" variant="primary" role="status" className="my-auto">
+                          <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                      </Dropdown.Item>
+                    )}
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
         </Form>
       </Container>
     </Container>
