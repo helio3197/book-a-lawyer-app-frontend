8c4f8d2623f23ff0d27876c234c3990027836562
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-14 18:55:18 +0300

    add git status

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import Welcome from '../home/Welcome';
 import Detail from './LawyerDetails';
 
 const Lawyer = () => {
   const userSignedIn = useSelector((state) => state.auth.userSignedIn);
+  const navigate = useNavigate;
+
+  useEffect(() => {
+    if (!userSignedIn) {
+      navigate('/', { state: { notice: 'Please sign in with your username and password' } });
+    }
+  }, []);
 
   return (
     <Container fluid className="p-0 h-100">
-      {userSignedIn
-        ? <Detail />
-        : <Welcome />}
+      <Detail />
     </Container>
   );
 };
