7cc05d11a9ed6e5742d2828158d594047dd96c79
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-14 19:02:30 +0300

    add reservations page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import Lawyer from './components/details/Details';
 import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
+import Reservations from './components/Reservations/Reservations';
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/lawyers/:id" element={<Lawyer />} />
+        <Route path="/reservations" element={<Reservations />} />
         <Route path="sign_up" element={<SignUp />} />
         <Route path="sign_in" element={<SignIn />} />
       </Route>
---
+++ src/components/Reservations/Reservations.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Reservations = () => (
+  <>
+    <h1>This is the Reservations page!</h1>
+  </>
+);
+
+export default Reservations;
