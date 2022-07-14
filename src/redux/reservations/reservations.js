83c90fdb7a000e9925d1f465095d2d4b071f2e1d
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-14 20:54:10 +0300

    add reservation reducer

---
+++ src/redux/reservations/reservations.js
@@ -0,0 +1,74 @@
+const API_RESERVATIONS_INDEX_ENDPOINT = `${process.env.REACT_APP_API_HOST}/api/v1/reservations`;
+const REQUEST_STARTED = 'book-a-lawyer/reservations/REQUEST_STARTED';
+const REQUEST_FAILED = 'book-a-lawyer/reservations/REQUEST_FAILED';
+const REQUEST_COMPLETED = 'book-a-lawyer/reservations/REQUEST_COMPLETED';
+const initialState = {
+  status: 'idle',
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_STARTED:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case REQUEST_FAILED:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case REQUEST_COMPLETED:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const requestStarted = () => ({
+  type: REQUEST_STARTED,
+  payload: {
+    status: 'fetching',
+  },
+});
+
+const requestFailed = (error) => ({
+  type: REQUEST_FAILED,
+  payload: {
+    status: 'failed',
+    error,
+  },
+});
+
+const requestCompleted = ({ reservation }) => ({
+  type: REQUEST_COMPLETED,
+  payload: {
+    status: 'completed',
+    reservation,
+  },
+});
+
+export const fechReservations = () => async (dispatch, getState) => {
+  dispatch(requestStarted());
+  try {
+    const { authToken } = getState().auth;
+    const response = await fetch(API_RESERVATIONS_INDEX_ENDPOINT, {
+      headers: {
+        Authorization: authToken,
+      },
+    });
+    console.log(await response.json());
+    if (!response.ok) {
+      throw (await response.json()).error;
+    }
+    // console.log(await response.json());
+    dispatch(requestCompleted(await response.json()));
+  } catch (error) {
+    dispatch(requestFailed(error));
+  }
+};
+
+export default reducer;
