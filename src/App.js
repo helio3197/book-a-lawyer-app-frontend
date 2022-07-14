75c5d57351c4be222dac55ba78e72ef907a91de2
Kenny <kennysalazar3197@gmail.com>
2022-07-14 12:03:27 -0500

    Update routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/lawyers/:id" element={<Lawyer />} />
+        <Route path="lawyers" element={<Home />} />
+        <Route path="lawyers/:id" element={<Lawyer />} />
         <Route path="sign_up" element={<SignUp />} />
         <Route path="sign_in" element={<SignIn />} />
       </Route>
