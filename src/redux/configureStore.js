106ccea638944b348881c147bc65ea6457547fe6
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-14 20:54:38 +0300

    configure reservations on store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/auth';
 import lawyersIndexReducer from './lawyers/lawyersIndex';
+import reservationList from './reservations/reservations';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     lawyers: lawyersIndexReducer,
+    reservations: reservationList,
   },
 });
 
