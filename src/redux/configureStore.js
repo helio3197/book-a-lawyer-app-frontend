ac9f36ceb929d871436ff060a121b21c91e8cc93
Kenny <kennysalazar3197@gmail.com>
2022-07-15 20:03:41 -0500

    Create edit user reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/auth';
 import lawyersIndexReducer from './lawyers/lawyersIndex';
 import reservationsCreate from './reservations/reservationsCreate';
+import usersEdit from './users/usersEdit';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     lawyers: lawyersIndexReducer,
     reservations_create: reservationsCreate,
+    users_edit: usersEdit,
   },
 });
 
