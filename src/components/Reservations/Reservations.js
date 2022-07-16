7532e5e2e8906d3b060fbe46c30a66a11bddb845
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-16 22:40:42 +0300

    add reservations loading

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -2,7 +2,10 @@
 import React, { useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
 import { fechReservations, deleteReservations } from '../../redux/reservations/reservations';
+import { getLawyers } from '../../redux/lawyers/lawyersIndex';
 
 const Reservations = () => {
   const dispatch = useDispatch();
@@ -15,13 +18,36 @@ const Reservations = () => {
     }
   }, []);
 
-  console.log(lawyersState.lawyers);
+  useEffect(() => {
+    if (lawyersState.statusLawyers !== 'completed') {
+      dispatch(getLawyers());
+    }
+  }, []);
+
   // console.log(reservationState);
 
   const deleteReservationData = (id) => () => {
     dispatch(deleteReservations(id));
   };
 
+  if (lawyersState.status === 'idle' || lawyersState.status === 'fetching') {
+    return (
+      <Container as="section" fluid className="py-2 lawyers align-items-center">
+        <h1 className="mt-3">Please Wait...</h1>
+        <Spinner animation="border" variant="primary" role="status" className="my-auto">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Container>
+    );
+  }
+
+  if (lawyersState.status === 'failed') {
+    return (
+      <Container as="section" fluid className="py-2 lawyers">
+        <h2 className="mt-2">{`Something went wrong: ${lawyersState.error}`}</h2>
+      </Container>
+    );
+  }
   return (
     <section className="reservationtop">
       {reservationState.reservations.map((reserve) => (
@@ -34,12 +60,18 @@ const Reservations = () => {
                 <p>Reservation Date</p>
                 <p>Reservation Time</p>
                 <p>Reservation Duration</p>
+                <p>Status</p>
               </div>
               <div>
                 <p>{lawyersState.lawyers.filter((item) => item.id === reserve.lawyer_id)[0].name}</p>
                 <p>{reserve.reservationdate.substring(0, 10)}</p>
                 <p>{reserve.reservationdate.substring(11, 16)}</p>
-                <p>{reserve.duration}</p>
+                <p>
+                  {reserve.duration}
+                  {' '}
+                  hrs
+                </p>
+                {reserve.is_active ? <p>Active</p> : <p>Inactive</p> }
               </div>
             </div>
             <div className="reservationbtns">
