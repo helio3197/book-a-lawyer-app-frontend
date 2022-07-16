628feba3956ff9df4927a7608d22550488f5ab3f
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-16 21:18:30 +0300

    add lawyers details

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,6 +7,7 @@ import { fechReservations, deleteReservations } from '../../redux/reservations/r
 const Reservations = () => {
   const dispatch = useDispatch();
   const reservationState = useSelector((state) => state.reservations);
+  const lawyersState = useSelector((state) => state.lawyers);
 
   useEffect(() => {
     if (reservationState.statusReservations !== 'completed') {
@@ -13,29 +15,37 @@ const Reservations = () => {
     }
   }, []);
 
-  const deleteReservationData = (id) => () => {;
+  console.log(lawyersState.lawyers);
+  // console.log(reservationState);
+
+  const deleteReservationData = (id) => () => {
     dispatch(deleteReservations(id));
   };
 
   return (
     <section className="reservationtop">
       {reservationState.reservations.map((reserve) => (
-        <div key={reserve.id} className="reservation">
-          <div className="reservationdeatails">
-            <div>
-              <p>Lawyers Name</p>
-              <p>Reservation Date</p>
-              <p>Reservation Time</p>
+        <div key={reserve.id} className="allinclusive">
+          <img src={lawyersState.lawyers.filter((item) => item.id === reserve.lawyer_id)[0].avatar_url} alt="Profle" className="lawyer-pic" />
+          <div key={reserve.id} className="reservation">
+            <div className="reservationdeatails">
+              <div>
+                <p>Lawyers Name</p>
+                <p>Reservation Date</p>
+                <p>Reservation Time</p>
+                <p>Reservation Duration</p>
+              </div>
+              <div>
+                <p>{lawyersState.lawyers.filter((item) => item.id === reserve.lawyer_id)[0].name}</p>
+                <p>{reserve.reservationdate.substring(0, 10)}</p>
+                <p>{reserve.reservationdate.substring(11, 16)}</p>
+                <p>{reserve.duration}</p>
+              </div>
             </div>
-            <div>
-              <p>{reserve.id}</p>
-              <p>{reserve.reservationdate.substring(0, 10)}</p>
-              <p>{reserve.reservationdate.substring(11, 16)}</p>
+            <div className="reservationbtns">
+              <Button className="button-custom">Edit</Button>
+              <Button onClick={deleteReservationData(reserve.id)}>Delete</Button>
             </div>
-          </div>
-          <div className="reservationbtns">
-            <Button className="button-custom">Edit</Button>
-            <Button onClick={deleteReservationData(reserve.id)}>Delete</Button>
           </div>
         </div>
       ))}
